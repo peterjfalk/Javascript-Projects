e67a80957dd91c21237851d836733565c796119c
Peter Falk <falkman13@gmail.com>
2022-05-02 14:08:06 -0400

    Basic function tests

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,5 @@
+function My_First_Function() {                                  //Function start
+    var str = "You FOOL!";                                      //Variable definition
+    str += " You fell victim to one of the classic blunders!"   //+= operator
+    document.getElementById("Button_Text").innerHTML = str;     //Function task
+}
