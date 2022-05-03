334908d122dacf8da76418b3aa87f84646053809
Peter Falk <falkman13@gmail.com>
2022-05-03 16:21:17 -0400

    Added delete and comments

--- Basic JavaScript Projects/Project4_dictionaries/JS/main.js
+++ Basic JavaScript Projects/Project4_dictionaries/JS/main.js
@@ -1,9 +1,10 @@
-function my_Dictionary() {
-    var Games = {
-        Name:"Minecraft",
+function my_Dictionary() {                  //Function calling
+    var Games = {                           //Define variable as a dictionary
+        Name:"Minecraft",                   //Dictionary listings
         Publisher:"Microsoft and Mojang",
         Genre:"Survival",
         Popularity:"High"
     };
-    document.getElementById("Dictionary").innerHTML = Games.Genre
+    delete Games.Genre                      //Delete dictionary listing
+    document.getElementById("Dictionary").innerHTML = Games.Genre   //Output a dictionary listing. Since it is deleted, it returns as undefined.
 }
