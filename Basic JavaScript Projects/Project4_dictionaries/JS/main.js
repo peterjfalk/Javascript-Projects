cd077b7cd58b3f81221894ba1546dce9b1b02c85
Peter Falk <falkman13@gmail.com>
2022-05-03 15:23:29 -0400

    First Dictionary test

---
+++ Basic JavaScript Projects/Project4_dictionaries/JS/main.js
@@ -0,0 +1,9 @@
+function my_Dictionary() {
+    var Games = {
+        Name:"Minecraft",
+        Publisher:"Microsoft and Mojang",
+        Genre:"Survival",
+        Popularity:"High"
+    };
+    document.getElementById("Dictionary").innerHTML = Games.Genre
+}
