1ad9954a75a82dc5b18e77878e295f508c845d46
Peter Falk <falkman13@gmail.com>
2022-05-28 20:37:44 -0400

    Added TicTacToe program

--- Coding-tests/JS/main.js
+++
@@ -1,6 +0,0 @@
-function rideFunction() {
-    var Height, canRide;
-    Height = document.getElementById("Height").value;
-    canRide = (Height < 52) ? "You are too short":"You are tall enough";
-    document.getElementById("Ride").innerHTML = canRide + " to ride.";
-};
